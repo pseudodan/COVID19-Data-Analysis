f42300a61059cde2604fe58a2d5bfb99f0fa0d02
DRenales <drena001@ucr.edu>
2020-11-19 18:09:09 -0800

    19 November 2020 EDIT Added these subroutines to the program: - verifyState: makes sure state input is valid - verifyCase: makes sure case result is valid - reformatInput: makes sure user input will always be readable in the query
    Added edits to the first two queries:
    - OPTION 1: Fixed to be in front of OPTION 2 for readability. Also included a case to display all case results. Added flexibility to user input
    - OPTION 2: Added flexibility to user input.

--- scalable-solution/COVID19-Data-Analysis/src/main/java/SparkWorks/USA_Queries.java
+++ scalable-solution/COVID19-Data-Analysis/src/main/java/SparkWorks/USA_Queries.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 public class USA_Queries {
     private static Dataset<Row> df;
     private static SparkSession sparkSession;
+    private static Scanner input = new Scanner(System.in);
 
     /*
         Function: Queries
@@ -21,11 +22,6 @@ public class USA_Queries {
         Summary: Constructor that sets the private data field Dataset<Row>
             and SparkSession. Afterwards, create the temporary view of the
             csv file that will be looked at.
-
-            LINE 1: Initialize SparkSession private data
-            LINE 2-7: Load the csv file from the hdfs file path provided
-                from SparkMainApp
-            LINE 8: Create temporary view that will be used by spark SQL
     */
     public USA_Queries(String filePath, SparkSession sparkSession)
     {
@@ -47,15 +43,9 @@ public class USA_Queries {
         Output: boolean
         Summary: Creates a buffered reader for the hdfs filepath to determine the
             validity of the state name chosen by a user.
-
-            LINE 1-3: Creates the necessary objects to load a buffered reader object
-            Line 4: Creates variable to hold value being read from the buffered reader
-            Line 6-8: While there are lines being read from the file, if an instance of
-                the state string is found in the file return true
-            Line 9: Return false if not able to find instance of state
     */
-    public static boolean verifyState(String state) throws Exception {
-        File f = new File("/home/spectre/Downloads/USA_States.txt");
+    private static boolean verifyState(String state) throws Exception {
+        File f = new File("/home/hdfs/USA_States.txt");
         FileReader fr = new FileReader(f);
         BufferedReader br = new BufferedReader(fr);
         String read;
@@ -66,6 +56,103 @@ public class USA_Queries {
         return false;
     }
 
+    /*
+        Function: verifyCase
+        Author: Dominic Renales
+        Editors:
+        Input: String
+        Output: boolean
+        Summary: Verifies that the chosen case result is valid
+    */
+    private static boolean verifyCase(String caseResult) {
+        switch(caseResult.toUpperCase()) {
+            case "POSITIVE":     case "P":
+            case "NEGATIVE":     case "N":
+            case "INCONCLUSIVE": case "I":
+            case "ALL":          case "A": return true;
+            default: return false;
+        }
+    }
+
+    /*
+        Function: reformatInput
+        Author: Dominic Renales
+        Editors:
+        Input: String
+        Output: String
+        Summary: Returns properly formatted input for flexibility's sake
+    */
+    private static String reformatInput(String state) {
+        if (state.length() == 1 || state.length() == 2) return state.toUpperCase();
+        return state.substring(0,1).toUpperCase() + state.substring(1).toLowerCase();
+    }
+
+    /*
+        Function: verifyDate
+        Author: Dominic Renales
+        Editors:
+        Input: String
+        Output: boolean
+        Summary:
+    */
+    public static boolean verifyDate(String date){
+        return false;
+    }
+
+    /* OPTION 1 COMPLETE [Query translated from non-scalable PSQL version]
+            Function: getNumSpecifiedOutcomesByState
+            Author: Daniel Murphy
+            Editors: Dominic Renales
+            Input: None
+            Output: None
+            Summary: Prints out the result of the specified outcome on a desired state.
+    */
+    public static void getNumOfSpecifiedOutcomesByState() throws Exception {
+        System.out.print("Which result would you like to view ([P]ositive/[N]egative/[I]nconclusive)/[A]ll): ");
+        String caseResult = input.nextLine();
+        while(!verifyCase(caseResult)) {
+            System.out.println("Invalid Input");
+            System.out.print("Which result would you like to view (Positive/Negative/Inconclusive: ");
+            caseResult = input.nextLine();
+        }
+        caseResult = reformatInput(caseResult);
+
+        System.out.print("Enter the desired state name: ");
+        String state = input.nextLine();
+        while(!verifyState(state.toUpperCase())) {
+            System.out.println("Invalid State Name.");
+            System.out.print("Enter the desired state name: ");
+            state = input.nextLine();
+        }
+        state = reformatInput(state);
+
+        if(!caseResult.equals("All") && !caseResult.equals("A")) {
+            if (state.length() == 2)
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '"
+                        + caseResult + "' AND state = '" + state + "';").show();
+            else
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '"
+                        + caseResult + "' AND state_name = '" + state + "';").show();
+        }
+        else {
+            if (state.length() == 2) {
+                System.out.println("POSITIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Positive" + "' AND state = '" + state + "';").show();
+                System.out.println("NEGATIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Negative" + "' AND state = '" + state + "';").show();
+                System.out.println("INCONCLUSIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Inconclusive" + "' AND state = '" + state + "';").show();
+            }
+            else {
+                System.out.println("POSITIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Positive" + "' AND state_name = '" + state + "';").show();
+                System.out.println("NEGATIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Negative" + "' AND state_name = '" + state + "';").show();
+                System.out.println("INCONCLUSIVE DATA:");
+                sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = '" + "Inconclusive" + "' AND state_name = '" + state + "';").show();
+            }
+        }
+    }
     /*  OPTION 2 COMPLETE [Query translated from non-scalable PSQL version]
         Function: getNumOfTestsAdministeredByState
         Author: Daniel Murphy
@@ -74,60 +161,8 @@ public class USA_Queries {
         Output: None
         Summary: Scans the USA data in the HDFS to print information regarding
             the number of tests conducted in a state
-
-            Line 1: Initialize scanner for input
-            Line 2-3: Prompt input and store it
-            Line 4-7: While the user is inputting invalid state names, keep
-                asking for input.
-            Line 8-9: If the state name used is abbreviation length search by
-                abbreviation (state) and then print the results
-            Line 10-11: Otherwise search by full state name (state_name) and
-                then print the results
     */
     public static void getNumOfTestsAdministeredByState() throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the desired state name: ");
-        String state = input.nextLine();
-
-        while(!verifyState(state.toUpperCase())) {
-            System.out.println("Invalid State Name.");
-            System.out.print("Enter the desired state name: ");
-            state = input.nextLine();
-        }
-        if(state.length() == 2)
-            sparkSession.sql("SELECT COUNT(*) FROM USA WHERE state =\'" + state + "\';").show();
-        else
-            sparkSession.sql("SELECT COUNT(*) FROM USA WHERE state_name =\'" + state + "\';").show();
-    }
-
-    /* OPTION 1 COMPLETE [Query translated from non-scalable PSQL version]
-        Function: getNumSpecifiedOutcomesByState
-        Author: Daniel Murphy
-        Editors: Dominic Renales
-        Input: None
-        Output: None
-        Summary: Prints out the result of the specified outcome on a desired state.
-            LINE 1: Initializes Scanner for user input
-            LINE 2-3: Prompt the user for input and store it
-            LINE 4-7: While the user input does not equal "Positive," "Negative,"
-                or "Inconclusive" keep prompting for user input
-            LINE 8-13: While the user input is not a valid state keep prompting the
-                user for input
-            LINE 14-15: Run query on state abbreviation
-            LINE 16-17: Run query on full state name
-
-    */
-    public static void getNumOfSpecifiedOutcomesByState() throws Exception {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Which result would you like to view (Positive/Negative/Inconclusive): ");
-        String choice = input.nextLine();
-
-        while(!choice.equals("Positive") && !choice.equals("Negative") && !choice.equals("Inconclusive")) {
-            System.out.println("Invalid Input");
-            System.out.print("Which result would you like to view (Positive/Negative/Inconclusive: ");
-            choice = input.nextLine();
-        }
-
         System.out.print("Enter the desired state name: ");
         String state = input.nextLine();
         while(!verifyState(state.toUpperCase())) {
@@ -135,12 +170,13 @@ public class USA_Queries {
             System.out.print("Enter the desired state name: ");
             state = input.nextLine();
         }
+        state= reformatInput(state);
 
         if(state.length() == 2)
-            sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = \'"
-                    + choice + "\' AND state = \'" + state + "\';").show(100,false);
+            sparkSession.sql("SELECT COUNT(*) FROM USA WHERE state ='" + state + "';").show();
         else
-            sparkSession.sql("SELECT COUNT(overall_outcome) FROM USA WHERE overall_outcome = \'"
-                    + choice + "\' AND state_name = \'" + state + "\';").show(100,false);
+            sparkSession.sql("SELECT COUNT(*) FROM USA WHERE state_name ='" + state + "';").show();
     }
+
+
 }
