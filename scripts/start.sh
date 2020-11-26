eb71acba24e2f80b5fdc383c6d0b788c653d32aa
Dan Murphy <43690299+pseudodan@users.noreply.github.com>
2020-11-25 19:43:47 -0800

    Modified scripts
    Split compile.sh into two scripts:
    1. start.sh
    2. run.sh
    
    => start.sh starts HDFS and Spark services
    => run.sh compiles and runs the project

---
+++ scripts/start.sh
@@ -0,0 +1,6 @@
+cd ~/hadoop-3.2.1/sbin
+./start-dfs.sh
+jps
+cd /home/linuxbrew/.linuxbrew/Cellar/apache-spark/3.0.1/libexec/sbin
+sudo ./start-master.sh
+sudo ./start-slave.sh spark://$hostname:7077
