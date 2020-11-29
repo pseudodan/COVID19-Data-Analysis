aeceaedc786c6cec854d17731fcd9e68e9b9da35
Dan Murphy <43690299+pseudodan@users.noreply.github.com>
2020-11-28 21:37:13 -0800

    [UPDATE] Scripts auto-return to home dir once completed
    QoL Update:
    End of each shell scrip returns you to your home dir, the easiest place to have your scripts run from.
    Hope this helps with faster testing!

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,3 +3,4 @@ mvn compile
 mvn package
 cd /home/linuxbrew/.linuxbrew/Cellar/apache-spark/3.0.1/bin
 sudo ./spark-submit --class SparkWorks.SparkMainApp ~/Documents/GitHub/COVID19-Data-Analysis/scalable-solution/COVID19-Data-Analysis/target/test-1.8-SNAPSHOT.jar --master spark://$hostname:7077
+cd ~
--- scripts/start.sh
+++ scripts/start.sh
@@ -4,3 +4,4 @@ jps
 cd /home/linuxbrew/.linuxbrew/Cellar/apache-spark/3.0.1/libexec/sbin
 sudo ./start-master.sh
 sudo ./start-slave.sh spark://$hostname:7077
+cd ~
